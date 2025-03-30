ca4165a02651e0066fc533e02b2e4c4d73d9428d
asfm4001 <asfm4001@yahoo.com.tw>
2025-03-30 13:18:45 +0800

    feat: add about/services/faqs/contact pages

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = 'pages'
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),
+     path('faqs/', views.faqs, name='faqs'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'pages/index.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+def faqs(request):
+    return render(request, 'pages/faqs.html')
