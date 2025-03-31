5bd80d9d4608ca986b875034b7292d81dcb60936
asfm4001 <asfm4001@yahoo.com.tw>
2025-03-31 14:48:22 +0800

    feat/refactor: add docker-compose, nginx(8888->80)

--- django/hydraulicFirm/settings.py
+++ django/hydraulicFirm/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,6 +121,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'pages/static',    # 非預設, 覆蓋django原始static路徑
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # 輸出app static files(for正式環境存取路徑)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
