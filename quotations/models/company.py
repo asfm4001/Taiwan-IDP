1428ec5944f11b5082f7fa2db503a6846fdcf5d1
asfm4001 <asfm4001@yahoo.com.tw>
2026-02-23 17:27:27 +0800

    fix(quotation): correct static file paths & set default static file path

--- quotations/models/company.py
+++ quotations/models/company.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 class Company(models.Model):
+    icon_default = 'quotations/assets/img/台整logo.png'
+    stamp_default = 'quotations/assets/img/台整stamp.png'
     title = models.CharField('公司名稱', max_length=40)
     address = models.CharField('地址', max_length=100, blank=True, null=True)
     phone = models.CharField('電話', max_length=20, blank=True, null=True)
     fax = models.CharField('傳真', max_length=20, blank=True, null=True)
     tax_code = models.CharField('統編', max_length=20, blank=True, null=True)
-    icon = models.CharField(max_length=255, blank=True, null=True)
-    stamp = models.CharField(max_length=255, blank=True, null=True)
+    icon = models.CharField(max_length=255, blank=True, null=True, default=icon_default)
+    stamp = models.CharField(max_length=255, blank=True, null=True, default=stamp_default)
     engineer_display = models.BooleanField('技師顯示', default=True)
     class Meta:
         verbose_name = '公司'
