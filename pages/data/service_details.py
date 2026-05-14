22e44f4076e13dca17e0bd903a67312f45ec9b7e
asfm4001 <asfm4001@yahoo.com.tw>
2026-05-14 12:10:21 +0800

    refactor(pages): add news_list into service_details

--- pages/data/service_details.py
+++ pages/data/service_details.py
@@ -8,10 +8,17 @@ SERVICE_DETAILS = {
     },
 
     "inspection": {
-        "title": "透水保水檢查",
-        "timing": "每年5月1日前...",
+        "title": "透水保水設施檢查",
+        "news_list": [
+            {
+                "type": "primary",
+                "title": "公告",
+                "context": '<span class="text-primary">依據透水保水自治條例: </span>「透水保水義務人依都市計畫規定設置透水保水相關貯留設施，應負維護責任」',
+            },
+        ],
+        "timing": '每年<span style="color: red;">5月1日</span>以前至少一次委託專業技術團隊維修、檢查，並維持正常運作，其有損壞或阻塞，應<span class="text-primary">立即修繕及清淤。</span>',
         "range": None,
-        "certificate": "附表二及附表三...",
+        "certificate": '受委託專業技術團隊依附表二填具及技師簽證。<span class="text-primary">協助附表二及附表三一併填報!</span>',
         "image": "/static/pages/assets/img/services/inspection/dm_v1.png",
     },
 
@@ -31,15 +38,38 @@ SERVICE_DETAILS = {
 
     "radar": {
         "title": "透地雷達探測",
-        "timing": "放樣勘驗前...",
-        "range": "基地四周20公尺...",
-        "certificate": "需技師簽證",
+        "news_list": [
+            {
+                "type": "danger",
+                "title": "NEW",
+                "context": '依據新北市政府工務局於114年3月12日發布之<span class="text-primary">「新北市政府辦理建築工程施工防救災計畫說明會作業原則」</span>，自114年4月1日起承造人應於施工前、後進行<span class="text-primary">透地雷達檢測</span>。',
+            },
+            {
+                "type": "danger",
+                "title": "NEW",
+                "context": '依據臺北市政府於113年1月10日發布之<span class="text-primary">「臺北市建築物施工中維護公共設施管理要點」</span>，自113年1月18日起承造人應於施工前、後進行<span class="text-primary">透地雷達檢測</span>。',
+            },
+        ],
+        "timing": '於<span class="text-primary">放樣勘驗 、竣工勘驗前</span>提送檢測報告',
+        "range": '基地四周<span class="text-primary">二十公尺</span>範圍內各項公共設施',
+        "certificate": '檢測報告書需檢附現況相片及<span class="text-primary">技師簽證</span>',
         "carousel_items": [
             {
                 "image_url": "pages/assets/img/透地雷達3.png",
-                "title": "GS9000",
+                "title": "瑞士PROCEQ-GS9000陣列式透地雷達",
+                "caption": "可輕易檢測出路面劣化及沉陷規模及範圍",
+            },
+            {
+                "image_url": "pages/assets/img/透地雷達1.png",
+                "title": "GS9000操作示意圖",
                 "caption": "鋪面劣化檢測",
-            }
-        ]
+            },
+            {
+                "image_url": "pages/assets/img/透地雷達2.png",
+                "title": "GS9000操作示意圖",
+                "caption": "鋪面劣化檢測",
+            },
+        ],
+        "scan_img": "pages/assets/img/radar/總圖.png",
     }
 }
